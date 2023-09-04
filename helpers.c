cd89952f0e41847d8039eebd096aff2092c787f2
nitzansaar <112202258+nitzansaar@users.noreply.github.com>
2023-09-03 19:03:59 -0700

    blur method finished

--- helpers.c
+++ helpers.c
@@ -1,11 +1,9 @@
 #include "helpers.h"
+#include "math.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    // go through each pixel in the image
-    // calculate the average for that pixel (int)(R + G + B) / 3
-    // set the new value for that pixel to the average
     int avg;
     for (int i = 0; i < height; i++)
     {
@@ -42,7 +40,49 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    RGBTRIPLE copy[height][width];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int totalRed = 0, totalGreen = 0, totalBlue = 0;
+            int count = 0;
+
+            // Iterate over neighboring pixels within a 3x3 grid
+            for (int row = -1; row <= 1; row++)
+            {
+                for (int col = -1; col <= 1; col++)
+                {
+                    int newRow = i + row;
+                    int newCol = j + col;
+
+                    // Check if the neighboring pixel is within bounds
+                    if (newRow >= 0 && newRow < height && newCol >= 0 && newCol < width)
+                    {
+                        totalRed += image[newRow][newCol].rgbtRed;
+                        totalGreen += image[newRow][newCol].rgbtGreen;
+                        totalBlue += image[newRow][newCol].rgbtBlue;
+                        count++;
+                    }
+                }
+            }
+
+            // Calculate the average color values and round
+            copy[i][j].rgbtRed = round((float)totalRed / count);
+            copy[i][j].rgbtGreen = round((float)totalGreen / count);
+            copy[i][j].rgbtBlue = round((float)totalBlue / count);
+        }
+    }
+
+    // Copy the blurred image back to the original image
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = copy[i][j];
+        }
+    }
 }
 
 // Detect edges
